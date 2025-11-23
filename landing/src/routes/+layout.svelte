1f1715741d8ce121f1a9621a94c7e9f8e8b93c4b
woragis <masteringthecode.woragis+github@gmail.com>
2025-11-23 16:39:36 -0300

    Add title and meta tags to landing page

--- landing/src/routes/+layout.svelte
+++ landing/src/routes/+layout.svelte
@@ -9,6 +9,15 @@
 </script>
 
 <svelte:head>
+	<title>Woragis - Software Development Agency</title>
+	<meta name="description" content="Woragis is a software development agency that specializes in building custom software solutions for businesses.">
+	<meta name="keywords" content="software development, software agency, software solutions, software development agency, software development company, software development services, software development company in Brazil, software development company in São Paulo, software development company in Rio de Janeiro, software development company in Belo Horizonte, software development company in Curitiba, software development company in Porto Alegre, software development company in Brasília, software development company in Fortaleza, software development company in Recife, software development company in Salvador, software development company in João Pessoa, software development company in Maceió, software development company in Aracaju, software development company in João Pessoa, software development company in Maceió, software development company in Aracaju, software development company in João Pessoa, software development company in Maceió, software development company in Aracaju">
+	<meta name="author" content="Woragis">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="index, follow">
+	<meta name="googlebot" content="index, follow">
+	<meta name="bingbot" content="index, follow">
+	<meta name="yandexbot" content="index, follow">
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
